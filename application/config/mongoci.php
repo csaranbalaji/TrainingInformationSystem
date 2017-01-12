38bd0f122072e217a8789af8ec31d02594d2b29b
Saran Balaji <csaranbalaji@gmail.com>
2017-01-12 13:40:29 +0530

    MongoDB integration

---
+++ application/config/mongoci.php
@@ -0,0 +1,9 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$config['mongo_server'] = 'localhost';
+$config['mongo_dbname'] = 'discussdesk';
+$config['mongo_auth']   = FALSE;
+$config['mongo_username'] = '';
+$config['mongo_password'] = '';
+
+?>
